45a30a088408780b688a139677f291256bf5c28f
xsiry <xsiries@gmail.com>
2017-03-22 19:21:12 +0800

    #构建目录

---
+++ static/notice/src/module.js
@@ -0,0 +1,22 @@
+define(function(require, exports, module) {
+	$.root_ = $('body');
+	module.exports = {
+		init: function() {
+			this._bindUI();
+		},
+		_bindUI: function() {
+			$.root_.on('click', '.attendance_bbtn', function(e) {
+      	window.location.href = '../apps/attendance.html';
+      })
+      $.root_.on("click", '.message_bbtn', function(e) {
+        window.location.href = '../apps/message.html';
+      })
+      $.root_.on("click", '.download_bbtn', function(e) {
+        window.location.href = '../apps/download.html';
+      })
+      $.root_.on("click", '.notice_bbtn', function(e) {
+        window.location.href = '../apps/notice.html';
+      })
+		}
+	}
+})
