0e9ba15ae3802b536aa18212fd6b20f43506b306
xsiry <xsiries@gmail.com>
2017-03-29 19:44:15 +0800

    #首页异步显示

--- static/index/src/module.js
+++ static/index/src/module.js
@@ -12,77 +12,85 @@ define(function(require, exports, module) {
       noticeCount();
     },
     _bindUI: function() {
-      // bind .name_search_btn
-      $.root_.on("click", '.loginout', function(e) {
-    	loginOut();
-      })
       $.root_.on("click", '.attendance_btn', function(e) {
-        window.location.href = 'apps/content.html#attendance';
+        $('.attendance_bbtn').click();
       })
       $.root_.on("click", '.message_btn', function(e) {
-        window.location.href = 'apps/content.html#message';
+        $('.message_bbtn').click();
       })
       $.root_.on("click", '.download_btn', function(e) {
-        window.location.href = 'apps/content.html#download';
+        $('.download_bbtn').click();
       })
       $.root_.on("click", '.notice_btn', function(e) {
-        window.location.href = 'apps/content.html#notice';
+        $('.notice_bbtn').click();
       })
     }
   };
 
   // Helpers
   function loadNetBarConfig() {
-	  $.ajax({
-	      type: 'GET',
-	      contentType: 'application/json',
-	      url: '/weChat/apps/loadIndexData.json',
-	      dataType: 'json',
-	      success: function(data) {
-	        if (data.success) {
-	        	var result = JSON.parse(data.result);
-	        	$('.netbar_icon').prop('src', 'static/images/u2646.png');
-	            $('.you-name').text(result.netbarName);
-	            $('.authorized_num').text(result.netbarTCount);
-	            $('.due_time').text('2018-02-15');
-	        }
-	      },
-	      error: function(e) {
-	        $('.login_msg').text(e);
-	      }
-	    });
-    
+    $.ajax({
+      type: 'GET',
+      contentType: 'application/json',
+      url: 'loadIndexData.json',
+      dataType: 'json',
+      success: function(data) {
+        if (data.success) {
+          var result = JSON.parse(data.result);
+          $('.netbar_icon').prop('src', '../static/images/u2646.png');
+          $('.you-name').text(result.netbarName);
+          $('.authorized_num').text(result.netbarTCount);
+          $('.due_time').text('2018-02-15');
+        }
+      },
+      error: function(e) {
+        $('.login_msg').text(e);
+      }
+    });
+
   }
 
   function messageCount() {
-    var count = 12;
-    if (count > 0) {
-      $('.message_count').addClass('bg-0');
-      $('.message_count').text(count);
-    }
-
+    var count = 0;
+    $.ajax({
+	      type: 'GET',
+	      contentType: 'application/json',
+	      url: 'getMsgs.json',
+	      dataType: 'json',
+	      success: function(data) {
+	        if (data.success) {
+	        	count = data.list.length;
+	        }
+	        if (count > 0) {
+	            $('.message_count').addClass('bg-0');
+	            $('.message_count').text(count);
+	          }
+	      },
+	      error: function(e) {
+	        console.log(e);
+	      }
+	    });
   }
 
   function noticeCount() {
-    var count = 12;
-    if (count > 0) {
-      $('.notice_count').addClass('bg-f');
-      $('.notice_count').text(count);
-    }
-  }
-  
-  function loginOut() {
-	    $.ajax({
+    var count = 0;
+    $.ajax({
 	      type: 'GET',
 	      contentType: 'application/json',
-	      url: 'loginOut.json',
+	      url: 'infoDelivery.json',
 	      dataType: 'json',
 	      success: function(data) {
-	    	  if (data) {
-	    		  window.location.href = 'login.html';
-	    	  }
+	        if (data.success) { 
+	        	count = data.list.length;
+	        }
+	        if (count > 0) {
+	            $('.notice_count').addClass('bg-f');
+	            $('.notice_count').text(count);
+	          }
 	      },
-	      error: function(e) {}
+	      error: function(e) {
+	        console.log(e);
+	      }
 	    });
-	  }
+  }
 })
